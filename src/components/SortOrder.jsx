95b5f5280f363b6f6ae4c097e9de125f393745a6
Tom H <tomghdev@gmail.com>
2024-05-10 10:20:48 +0100

    modifies sort tab text to more use friendly

--- src/components/SortOrder.jsx
+++ src/components/SortOrder.jsx
@@ -4,14 +4,14 @@ function SortOrder({setSearchParams, setSortBy, setOrderBy}){
     return <div className="sort-and-order-div">
             <span>Sort By: </span>
         <div className="sort-by-div">
-            <p onClick={()=>{setSortBy("created_at")}}>created_at</p>
-            <p onClick={()=>{setSortBy("comment_count")}}>comment_count</p>
-            <p onClick={()=>{setSortBy("votes")}}>votes</p>
+            <p onClick={()=>{setSortBy("created_at")}}>Date</p>
+            <p onClick={()=>{setSortBy("comment_count")}}>Comments</p>
+            <p onClick={()=>{setSortBy("votes")}}>Votes</p>
         </div>
             <span>Order By: </span>
         <div className="order-by-div">
-            <p onClick={()=>{setOrderBy("ASC")}}>ASC</p>
-            <p onClick={()=>{setOrderBy("DESC")}}>DESC</p>
+            <p onClick={()=>{setOrderBy("ASC")}}>Ascending</p>
+            <p onClick={()=>{setOrderBy("DESC")}}>Descending</p>
         </div>
     </div>
 }

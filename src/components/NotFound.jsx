0edf4add7034ff6eca83a288291792f3a011a730
Tom H <tomghdev@gmail.com>
2024-05-10 10:16:43 +0100

    adds page not found 404 to all unknown routes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,13 @@
+import { NcLogo } from "./NcLogo"
+
 function NotFound() {
-    return <h2>Not Found: 404</h2>
-}
+    return (
+        <div className="center-items">
+            <h2 className="not-found">404:</h2>
+            <h3>Page Not Found</h3>
+            <NcLogo />
+        </div>
+        )
+    }
 
 export default NotFound
